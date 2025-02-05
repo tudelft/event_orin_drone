7ad06ff1d1cee64b9f3500a254cb558f1009da36
Huizerd <15855769+Huizerd@users.noreply.github.com>
2025-02-05 12:04:04 +0100

    baudrate back to 3mbps

--- orin/drone_flight.sh
+++ orin/drone_flight.sh
@@ -5,7 +5,7 @@ root_dir=$HOME/projects
 # mavlink-router
 tmux new-session -d -s drone_flight "mavlink-routerd"
 # micro XRCE-DDS agent
-tmux split-window "micro-xrce-dds-agent serial --dev /dev/ttyTHS3 -b 1500000"
+tmux split-window "micro-xrce-dds-agent serial --dev /dev/ttyTHS3 -b 3000000"
 # empty container
 tmux split-window "cd $root_dir/<container_folder> && jetson-containers run -v .:/workspace --user $(id -u):$(id -g) <container_name>"
 # htop (jtop not working?)
